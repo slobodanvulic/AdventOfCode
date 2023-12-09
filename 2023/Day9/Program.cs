ab8531deeb780394e0b56b382e149cffba0349e5
Slobodan Vulic <v.slobo92@gmail.com>
2023-12-09 07:08:07 +0100

    AoC 2023 - solutions day 9

---
+++ 2023/Day9/Program.cs
@@ -0,0 +1,41 @@
+﻿// Task https://adventofcode.com/2023/day/9
+Console.WriteLine("AoC 2023 - Day 9");
+
+var inputData = File.ReadAllLines("inputValues1.txt");
+var asd = Extrapolate(inputData[2].Split(' ').Select(int.Parse).ToList());
+
+var firstPart = inputData
+    .Select(line => line.Split(' ').Select(int.Parse))
+    .Select(sequence => Extrapolate(sequence.ToList()))
+    .Sum();
+
+Console.WriteLine($"First part: {firstPart}");
+
+var secondPart = inputData
+    .Select(line => line.Split(' ').Select(int.Parse))
+    .Select(sequence => ExtrapolateBackwards(sequence.ToList()))
+    .Sum();
+
+Console.WriteLine($"Second part: {secondPart}");
+Console.ReadKey();
+
+static int Extrapolate(List<int> sequence)
+{
+    if (sequence.All(n => n == 0))
+        return 0;
+
+    return Extrapolate(NextDifference(sequence)) + sequence[^1];
+}
+
+static int ExtrapolateBackwards(List<int> sequence)
+{
+    if (sequence.All(n => n == 0))
+        return 0;
+
+    return sequence[0] - ExtrapolateBackwards(NextDifference(sequence));
+}
+
+static List<int> NextDifference(List<int> sequence)
+{
+    return sequence.Skip(1).Select((x, i) => x - sequence[i]).ToList();
+}
